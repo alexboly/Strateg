a4b5550d24a684ab637182e86352c2150f659ce5
Alexandru Bolboaca <alexboly@gmail.com>
2012-12-27 14:01:22 +0200

    Adds a computer player

--- tests/features/Test.feature
+++ tests/features/Test.feature
@@ -6,3 +6,7 @@ Feature: Start game
 	Scenario: No player
 		When I configure the game
 		Then there's no player
+		
+	Scenario: Add computer player
+		When I add a computer player
+		Then there's one player
--- tests/features/steps.py
+++ tests/features/steps.py
@@ -1,10 +1,23 @@
 from lettuce import step
 
+playersCount = 0
+computerPlayers = 0
+
 @step(u'When I configure the game')
 def when_i_configure_the_game(step):
     pass
 
 @step(u'Then there\'s no player')
 def then_there_s_no_player(step):
-    playersCount = 0
     assert playersCount == 0 
+
+@step(u'When I add a computer player')
+def when_i_add_a_computer_player(step):
+    global computerPlayers
+    global playersCount
+    computerPlayers += 1
+    playersCount += computerPlayers
+
+@step(u'Then there\'s one player')
+def then_there_s_one_player(step):
+    assert playersCount == 1
